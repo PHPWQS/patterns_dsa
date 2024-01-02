47ad081a5c0b21f512dae3b850a2a726346900de
PHPWQS <mesroparakelyan1@gmail.com>
2024-01-02 18:27:23 +0400

    prototype pattern finish

--- patterns/prototype.php
+++ patterns/prototype.php
@@ -1,1 +1,25 @@
 <?php
+
+class SomeClass
+{
+  public string $name;
+  public string $lastname;
+
+  public function __construct(string $name, string $lastname)
+  {
+    $this->name = $name;
+    $this->lastname = $lastname;
+  }
+
+  public function get_fullname() : string {
+    return $this->name . " " . $this->lastname . "\n";
+  }
+  
+  public function __clone()
+  {
+    return new self($this->name, $this->lastname);
+  }
+}
+
+$some_class = new SomeClass("Mesrop", "Arakelyan");
+echo $some_class->get_fullname();
