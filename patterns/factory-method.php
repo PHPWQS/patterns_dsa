88856f565558e7521e77e6e6750baf4ed136957a
PHPWQS <mesroparakelyan1@gmail.com>
2024-01-02 18:16:04 +0400

    first patterns

---
+++ patterns/factory-method.php
@@ -0,0 +1,73 @@
+<?php
+
+class ShipLogistic
+{
+  public string $product_type;
+  public float $product_weight;
+
+  private static float $price = 22.3;
+
+  public function __construct(string $product_type, string $product_weight)
+  {
+    $this->product_type = $product_type;
+    $this->product_weight = $product_weight;
+  }
+
+  public function calculate(): string
+  {
+    $price = $this->product_weight * self::$price;
+
+    return "$price $";
+  }
+}
+
+class PlaneLogistic
+{
+  public string $product_type;
+  public float $product_weight;
+
+  private static float $price = 44;
+
+  public function __construct(string $product_type, string $product_weight)
+  {
+    $this->product_type = $product_type;
+    $this->product_weight = $product_weight;
+  }
+
+  public function calculate(): string
+  {
+    $price = $this->product_weight * self::$price;
+
+    return "$price $";
+  }
+}
+
+class LogisticFactory
+{
+  public string $type;
+  public mixed $instance;
+  
+  public function __construct(string $type, string $product, float $weight)
+  {
+    $this->type = $type;
+    
+    if ($type == 'plane') {
+      $this->instance = new PlaneLogistic($product, $weight);
+    }elseif ($type === 'ship') {
+      $this->instance = new ShipLogistic($product, $weight);
+    }else {
+      throw new Error("Sorry but $type not supports.");
+    }
+  }
+
+  public function getInstance()
+  {
+    return $this->instance;
+  }
+}
+
+$ship_logistic = (new LogisticFactory('ship', 'Nike boots', 224))->getInstance();
+$plane_logistic = (new LogisticFactory('plane', 'Iphone 15', 333))->getInstance();
+
+var_dump($ship_logistic->calculate());
+var_dump($plane_logistic->calculate());
---
+++ patterns/singleton.php
@@ -0,0 +1,29 @@
+<?php
+
+class DatabaseSingleton
+{
+  private static self $instance;
+
+  public readonly string $pdo;
+  
+  public function __construct(string $pdo)
+  {
+    $this->pdo = $pdo;  
+  }
+
+  public static function getInstance(string $pdo): self
+  {
+    if (isset(self::$instance)) {
+      return self::$instance;
+    }
+
+    self::$instance = new self($pdo);
+    return self::$instance;
+  }
+}
+
+
+$db_example_1 = DatabaseSingleton::getInstance('mysql:host=localhost;username=postgres;password=1234');
+$db_example_2 = DatabaseSingleton::getInstance('psql:host=localhost;username=postgres;password=1234');
+
+echo $db_example_1->pdo, $db_example_2->pdo;
